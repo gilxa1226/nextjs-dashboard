6b08a05fcd3d7e29afdc34d5e6954c569c9a8d44
Michael Haller <mike.haller@gmail.com>
2025-02-15 21:09:15 -0500

    completed the customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,27 @@
 import { Metadata } from 'next';
- 
+import { Suspense } from 'react';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
 export const metadata: Metadata = {
     title: 'Customers',
 }
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '%'
+    const formattedCustomers = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <Suspense>
+                <CustomersTable customers={formattedCustomers} />
+            </Suspense>
+        </div>
+    )
 }
